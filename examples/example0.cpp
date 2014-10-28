72af0287bb68995815d768f3fa37a1f436571777
Alan Livio <alan@telemidia.puc-rio.br>
2014-10-28 15:21:00 -0200

    maint: Add pkg-config test for libplatinumkit in configure.ac. Turn examples/example0.cpp in simple libplatinumkit test.

--- examples/example0.cpp
+++ examples/example0.cpp
@@ -1,7 +1,35 @@
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Platinum.h"
+#include "PltMediaConnect.h"
+#include "PltSyncMediaBrowser.h"
 
-using namespace std;
+int
+main (void)
+{
+  // setup Neptune logging
+  NPT_LogManager::GetDefault ().Configure (
+      "plist:.level=INFO;.handlers=ConsoleHandler;.ConsoleHandler.colors=off;.ConsoleHandler.filter=58");
 
-int main() {
+  PLT_UPnP upnp;
+  PLT_CtrlPointReference ctrlPoint (new PLT_CtrlPoint ());
+  upnp.AddCtrlPoint (ctrlPoint);
+
+  upnp.Start ();
+
+  // extra broadcast discover
+  ctrlPoint->Discover (NPT_HttpUrl ("255.255.255.255", 1900, "*"),
+		       "upnp:rootdevice", 1);
+
+  char buf[256];
+  while (gets (buf))
+    {
+      if (*buf == 'q')
+	break;
+    }
+
+  upnp.Stop ();
+
   return 0;
 }
