fc2e808aa9b6006eda0a7183244c707228bb1162
Alan Livio <alan@telemidia.puc-rio.br>
2014-10-28 17:47:23 -0200

    maint: Convert examples/example1.cpp in tests/test0.cpp for make check.

--- tests/test0.cpp
+++ tests/test0.cpp
@@ -1,8 +1,26 @@
 #include <cassert>
 #include <cstring>
+#include <cstdlib>
 
-using namespace std;
+#include "fsmda/services/upnp_service.h"
+#include "fsmda/model/communication/ondemand_object_control.h"
+#include "fsmda/model/communication/active_object_control.h"
+#include "fsmda/model/communication/passive_object_control.h"
+#include "fsmda/model/communication/mediacapture_object_control.h"
+#include "fsmda/model/paring/class_handling.h"
+#include "fsmda/model/paring/device_paring.h"
+#include "fsmda/gingancl/paring/parent_paring_manager.h"
+#include "fsmda/gingancl/paring/child_paring_manager.h"
 
-int main() {
-  return 0;
+int
+main (int argc, char **argv)
+{
+  ParentParingManager * ppm = new ParentParingManager ();
+  assert(ppm != NULL);
+  ChildParingManager * cpm = new ChildParingManager ();
+  assert(cpm != NULL);
+  delete ppm;
+  delete cpm;
+
+  return EXIT_SUCCESS;
 }
