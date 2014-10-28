f5c4873840a0522dd98831c5d37cbd053412f237
Alan Livio <alan@telemidia.puc-rio.br>
2014-10-28 17:07:02 -0200

    maint: move child_paring_manager.cp adn parent_paring_manager.cpp files to fsmda/gingancl/paring/

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -5,8 +5,8 @@
 #include "fsmda/model/communication/mediacapture_object_control.h"
 #include "fsmda/model/paring/class_handling.h"
 #include "fsmda/model/paring/device_paring.h"
-#include "fsmda/gingancl/parent_paring_manager.h"
-#include "fsmda/gingancl/child_paring_manager.h"
+#include "fsmda/gingancl/paring/parent_paring_manager.h"
+#include "fsmda/gingancl/paring/child_paring_manager.h"
 #include <cstdlib>
 
 int
