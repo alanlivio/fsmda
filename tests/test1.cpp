3e00b47254c0422c979e7e714fe230ef2c708525
Alan Livio <alan@telemidia.puc-rio.br>
2014-10-30 09:45:25 -0200

    maint: Increment tests/test1.cpp with more rdf parse tests.

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -7,15 +7,79 @@
 int
 main (int argc, char **argv)
 {
-  string device_rdf ("device_description000.xml");
+  string device_rdf;
+  string device_class_description_rdf;
+  bool ret;
   DeviceDescription * device_description = new DeviceDescription ();
+  DeviceClassDescription * device_class_description =
+      new DeviceClassDescription ();
+
+  device_rdf = "./files/test1_passive_dev_desc00.xml";
   device_description->initialize_by_rdf_file (device_rdf);
   assert(device_description != NULL);
-  string device_class_description_rdf ("device_class_description000.xml");
-  DeviceClassDescription * device_class_description = new DeviceClassDescription();
-  device_class_description->initialize_by_parse_rdf_file (device_class_description_rdf);
+
+  device_class_description_rdf = "./files/test1_passive_class_desc00.xml";
+  device_class_description->initialize_by_parse_rdf_file (
+      device_class_description_rdf);
   assert(device_class_description != NULL);
-  bool ret = device_class_description->device_meets_requirements(device_description);
+
+  ret = device_class_description->device_meets_requirements (
+      device_description);
   assert(ret);
+
+  device_rdf = "./files/test1_active_dev_desc00.xml";
+  device_description->initialize_by_rdf_file (device_rdf);
+  assert(device_description != NULL);
+
+  device_class_description_rdf = "./files/test1_active_class_desc00.xml";
+  device_class_description->initialize_by_parse_rdf_file (
+      device_class_description_rdf);
+  assert(device_class_description != NULL);
+
+  ret = device_class_description->device_meets_requirements (
+      device_description);
+  assert(ret);
+
+  device_rdf = "./files/test1_html_dev_desc00.xml";
+  device_description->initialize_by_rdf_file (device_rdf);
+  assert(device_description != NULL);
+
+  device_class_description_rdf = "./files/test1_html_class_desc00.xml";
+  device_class_description->initialize_by_parse_rdf_file (
+      device_class_description_rdf);
+  assert(device_class_description != NULL);
+
+  ret = device_class_description->device_meets_requirements (
+      device_description);
+  assert(ret);
+
+  device_rdf = "./files/test1_ondemand_dev_desc00.xml";
+  device_description->initialize_by_rdf_file (device_rdf);
+  assert(device_description != NULL);
+
+  device_class_description_rdf = "./files/test1_ondemand_class_desc00.xml";
+  device_class_description->initialize_by_parse_rdf_file (
+      device_class_description_rdf);
+  assert(device_class_description != NULL);
+
+  ret = device_class_description->device_meets_requirements (
+      device_description);
+  assert(ret);
+
+  device_rdf = "./files/test1_mediacapture_dev_desc00.xml";
+  device_description->initialize_by_rdf_file (device_rdf);
+  assert(device_description != NULL);
+
+  device_class_description_rdf = "./files/test1_mediacapture_class_desc00.xml";
+  device_class_description->initialize_by_parse_rdf_file (
+      device_class_description_rdf);
+  assert(device_class_description != NULL);
+
+  ret = device_class_description->device_meets_requirements (
+      device_description);
+  assert(ret);
+
+
+
   return EXIT_SUCCESS;
 }
