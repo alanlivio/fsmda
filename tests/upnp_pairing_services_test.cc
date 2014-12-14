d7b8b676e771927936fd573fb53b553afab0ff23
Alan Livio <alan@telemidia.puc-rio.br>
2014-12-14 18:25:09 -0200

    tests: Refactory upnp_pairing_services_test.cc and create PairingHandShakeHelper(bool).

--- tests/upnp_pairing_services_test.cc
+++ tests/upnp_pairing_services_test.cc
@@ -19,7 +19,7 @@ using std::endl;
 using std::fstream;
 using std::ifstream;
 
-void HandShakeInSameProcessHelper() {
+void PairingHandShakeHelper(bool diferent_processes) {
   UpnpParentPairing* upnp_parent_pairing;
   UpnpChildPairing* upnp_child_pairing;
 
@@ -33,31 +33,40 @@ void HandShakeInSameProcessHelper() {
   EXPECT_TRUE(upnp_child_pairing->IsPairingServiceStarted());
   EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 1);
 
-  // start parent pairing service with fake discover params
-  // only for enable handshake
-  upnp_parent_pairing = new UpnpParentPairing();
-  string application_id = "fake-application-id";
-  unsigned int class_index = 2;
-  DeviceClassDescription* device_class_description =
-      new DeviceClassDescription();
-  device_class_description->InitializeByDeviceClass(
-      DeviceClassDescription::kActiveDevice);
-  DeviceClassDiscoverParams* dicover_params = new DeviceClassDiscoverParams(
-      application_id, class_index, device_class_description);
-  upnp_parent_pairing->AddDeviceClassForDiscover(dicover_params);
-  EXPECT_EQ(upnp_parent_pairing->StartPairingService(), 0);
-  EXPECT_TRUE(upnp_parent_pairing->IsPairingServiceStarted());
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 2);
-
+  if (diferent_processes) {
+    // start parent pairing service
+    FILE* parent_pipe = popen("./fake_parent_helper", "w");
+    ASSERT_TRUE(parent_pipe);
+    fprintf(parent_pipe, "%s\n", "active");
+    fclose(parent_pipe);
+  } else {
+    // start parent pairing service with fake discover params
+    // only for enable handshake
+    upnp_parent_pairing = new UpnpParentPairing();
+    string application_id = "fake-application-id";
+    unsigned int class_index = 2;
+    DeviceClassDescription* device_class_description =
+        new DeviceClassDescription();
+    device_class_description->InitializeByDeviceClass(
+        DeviceClassDescription::kActiveDevice);
+    DeviceClassDiscoverParams* dicover_params = new DeviceClassDiscoverParams(
+        application_id, class_index, device_class_description);
+    upnp_parent_pairing->AddDeviceClassForDiscover(dicover_params);
+    EXPECT_EQ(upnp_parent_pairing->StartPairingService(), 0);
+    EXPECT_TRUE(upnp_parent_pairing->IsPairingServiceStarted());
+    EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 2);
+  }
   // test if child is paired
   sleep(1);
   EXPECT_TRUE(upnp_child_pairing->PerformedHandShake());
 
-  // stop parent pairing service
-  EXPECT_EQ(upnp_parent_pairing->StopPairingService(), 0);
-  EXPECT_FALSE(upnp_parent_pairing->IsPairingServiceStarted());
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 1);
-  delete upnp_parent_pairing;
+  if (diferent_processes == false) {
+    // stop parent pairing service
+    EXPECT_EQ(upnp_parent_pairing->StopPairingService(), 0);
+    EXPECT_FALSE(upnp_parent_pairing->IsPairingServiceStarted());
+    EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 1);
+    delete upnp_parent_pairing;
+  }
 
   // stop child pairing service
   EXPECT_EQ(upnp_child_pairing->StopPairingService(), 0);
@@ -70,44 +79,10 @@ void HandShakeInSameProcessHelper() {
   EXPECT_FALSE(UpnpFsmdaUtils::IsUpnpStarted());
 }
 
-void ChildHandShakeInDiferentProcessesHelper() {
-  UpnpChildPairing* upnp_child_pairing;
-
-  // test if upnp is running
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 0);
-  EXPECT_FALSE(UpnpFsmdaUtils::IsUpnpStarted());
-
-  // start child pairing service
-  upnp_child_pairing = new UpnpChildPairing();
-  EXPECT_EQ(upnp_child_pairing->StartPairingService(), 0);
-  EXPECT_TRUE(upnp_child_pairing->IsPairingServiceStarted());
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 1);
-
-  // start parent pairing service
-  FILE* parent_pipe = popen("./fake_parent_helper", "w");
-  ASSERT_TRUE(parent_pipe);
-  fprintf(parent_pipe, "%s\n", "active");
-  fclose(parent_pipe);
-
-  // test if child is paired
-  sleep(1);
-  EXPECT_TRUE(upnp_child_pairing->PerformedHandShake());
-
-  // stop child pairing service
-  EXPECT_EQ(upnp_child_pairing->StopPairingService(), 0);
-  EXPECT_FALSE(upnp_child_pairing->IsPairingServiceStarted());
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 0);
-  delete upnp_child_pairing;
-
-  // test if upnp is running
-  EXPECT_EQ(UpnpFsmdaUtils::upnp_references_count(), 0);
-  EXPECT_FALSE(UpnpFsmdaUtils::IsUpnpStarted());
+TEST(UpnpPairingServicesTest, HandShakeInSameProcess) {
+  PairingHandShakeHelper(false);
 }
 
-TEST(UpnpPairingServicesTest, ChildHandShakeInSameProcess) {
-  HandShakeInSameProcessHelper();
-}
-
-TEST(UpnpPairingServicesTest, ChildHandShakeInDiferentProcesses) {
-  ChildHandShakeInDiferentProcessesHelper();
+TEST(UpnpPairingServicesTest, HandShakeInDiferentProcesses) {
+  PairingHandShakeHelper(true);
 }
