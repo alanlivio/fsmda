785a89d28c89e04d1ff7c239c07965e9c5dc4b2f
Alan Livio <alan@telemidia.puc-rio.br>
2015-01-08 09:02:57 -0200

    tests: Refactory fsmda_profiling.sh output.

--- tests/fsmda_profiling.sh
+++ tests/fsmda_profiling.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
-#profilie configuration
+### profilie configuration
 n=2
 declare -a classes=("active" "passive" "mediacapture" "ondemand")
-declare -a profiles=("profile_pairing" "profile_prepare" "profile_command" "profile_variable" "profile_remove_device" "profile_bufferd_command")
+declare -a profiles=("profile_pairing" "profile_remove_device" \
+  "profile_prepare" "profile_command" \
+  "profile_variable"  "profile_bufferd_command")
 app_id=dc05b236-0cce-4f1d-996b-edd11a66d907
 
-# redirect erros to /dev/null
+### redirect erros to /dev/null
 #exec 1>> /dev/null
 exec 2>> /dev/null
 
-# clean logs
+### clean logs
 rm fsmda_profiling.log
 
-# profiling
-for profile in "${profiles[@]}";do
-    for class in "${classes[@]}";do
-        echo -n "Running $n instances of $profile for $class class... average="
-        rm -rf fsmda_pairing_profile.log
-        for ((i=1; i<=$n; i++));do
-          ./release_fake_child_parent.sh
-          ./fake_parent_helper --device_class=$class --application_id=$app_id --$profile >> fsmda_profiling.log &
-          ./fake_child_helper --device_class=$class --application_id=$app_id --$profile >> fsmda_profiling.log
-          ./release_fake_child_parent.sh
-        done
-        grep "fsmda_profiling_child $profile $class" < fsmda_profiling.log |awk '{ SUM += $4; N += 1} END { print SUM/N }'
+### profiling
+for class in "${classes[@]}";do
+    ### profile_prepare
+    echo "Running $n times of profile_prepare for $class class..."
+    rm -rf fsmda_pairing_profile.log
+    for ((i=1; i<=$n; i++));do
+      ./release_fake_child_parent.sh
+      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_pairing \
+         >> fsmda_profiling.log &
+      ./fake_child_helper --device_class=$class --application_id=$app_id --profile_pairing \
+         >> fsmda_profiling.log
+      ./release_fake_child_parent.sh
     done
+    grep "fsmda_profiling_child profile_pairing $class" < fsmda_profiling.log \
+     |awk '{ SUM += $4; N += 1} END { printf "child_average=%d\n",SUM/N}'
+    grep "fsmda_profiling_parent profile_pairing $class" < fsmda_profiling.log \
+     |awk '{ SUM += $4; N += 1} END { printf "parent_average=%d\n",SUM/N }'
 done
