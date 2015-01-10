439e41bbdbac22451cf11b84dd3fba3b561222d7
Alan Livio <alan@telemidia.puc-rio.br>
2015-01-10 18:00:58 -0200

    tests: Fix release_fake_child_parent.sh.

--- tests/release_fake_child_parent.sh
+++ tests/release_fake_child_parent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-killall lt-fake_parent_helper fake_parent_helper lt-fake_parent_helper fake_parent_helper &> /dev/null
+killall lt-fake_child_helper lt-fake_parent_helper fake_parent_helper fake_child_helper &> /dev/null
 rm -rf /dev/shm/sem.fsmda_semaphore_* 
 exit 0
