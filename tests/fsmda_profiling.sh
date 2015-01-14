a3a18615997fe282aec2384deba97464c230f41d
Alan Livio <alan@telemidia.puc-rio.br>
2015-01-14 14:47:49 -0200

    tests: Refactory fsmda_profiling.sh.

--- tests/fsmda_profiling.sh
+++ tests/fsmda_profiling.sh
@@ -11,14 +11,16 @@ exec 2> /dev/null
 ### release old log
 rm fsmda_profiling.log
 
+### intial release
+./release_fake_child_parent.sh  && ./release_gtest.sh
+
 ### profile_pairing  ########################################
 echo "### Running $n times profile_pairing ###"
 for class in "active" "passive" "mediacapture" "ondemand";do
     for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
+      ./release_fake_child_parent.sh
       ./fake_parent_helper --device_class=$class --application_id=$app_id \
          >> fsmda_profiling.log &
-      sleep 0.5;
       ./fake_child_helper --device_class=$class --profile_pairing \
          >> fsmda_profiling.log
     done
@@ -27,14 +29,60 @@ for class in "active" "passive" "mediacapture" "ondemand";do
       |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N}'
 done
 
+### profile_prepare  ########################################
+echo "### Running $n times profile_prepare ###"
+for class in "active";do
+    for ((i=1; i<=$n; i++));do
+      ./release_fake_child_parent.sh
+      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_prepare \
+         >> fsmda_profiling.log &
+      ./fake_child_helper --device_class=$class \
+         >> fsmda_profiling.log
+    done
+    echo -n "$class parent average="
+    grep "fsmda_parent profile_prepare $class" < fsmda_profiling.log \
+      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
+done
+
+### profile_command  ########################################
+echo "### Running $n times profile_command ###"
+for class in "active";do
+    for ((i=1; i<=$n; i++));do
+      ./release_fake_child_parent.sh
+      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_command \
+         >> fsmda_profiling.log &
+      ./fake_child_helper --device_class=$class \
+         >> fsmda_profiling.log
+    done
+    echo -n "$class parent average="
+    grep "fsmda_parent profile_command $class" < fsmda_profiling.log \
+      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
+done
+
+
+
+### profile_variable  ########################################
+echo "### Running $n times profile_variable ###"
+for class in "active";do
+    for ((i=1; i<=$n; i++));do
+      ./release_fake_child_parent.sh
+      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_variable \
+         >> fsmda_profiling.log &
+      ./fake_child_helper --device_class=$class\
+         >> fsmda_profiling.log
+    done
+    echo -n "$class parent average="
+    grep "fsmda_parent profile_variable $class" < fsmda_profiling.log \
+      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
+done
+
 ### profile_remove_device ########################################
 echo "### Running $n times profile_remove_device ###"
 for class in "active" "passive" "mediacapture" "ondemand";do
     for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
+      ./release_fake_child_parent.sh
       ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_remove_device \
          >> fsmda_profiling.log &
-      sleep 0.5;
       ./fake_child_helper --device_class=$class\
          >> fsmda_profiling.log
     done
@@ -47,10 +95,9 @@ done
 echo "### Running $n times profile_bufferd_command ###"
 for class in "active";do
     for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
+      ./release_fake_child_parent.sh
       ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_bufferd_command \
          >> fsmda_profiling.log &
-      sleep 0.5;
       ./fake_child_helper --device_class=$class --profile_bufferd_command \
          >> fsmda_profiling.log
     done
@@ -62,55 +109,6 @@ for class in "active";do
       |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N}'
 done
 
-### profile_command  ########################################
-echo "### Running $n times profile_command ###"
-for class in "active";do
-    for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
-      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_command \
-         >> fsmda_profiling.log &
-         sleep 0.5;
-      ./fake_child_helper --device_class=$class \
-         >> fsmda_profiling.log
-    done
-    echo -n "$class parent average="
-    grep "fsmda_parent profile_command $class" < fsmda_profiling.log \
-      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
-done
-
-
-### profile_prepare  ########################################
-echo "### Running $n times profile_prepare ###"
-for class in "active";do
-    for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
-      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_prepare \
-         >> fsmda_profiling.log &
-         sleep 0.5;
-      ./fake_child_helper --device_class=$class \
-         >> fsmda_profiling.log
-    done
-    echo -n "$class parent average="
-    grep "fsmda_parent profile_prepare $class" < fsmda_profiling.log \
-      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
-done
-
-### profile_variable  ########################################
-echo "### Running $n times profile_variable ###"
-for class in "active";do
-    for ((i=1; i<=$n; i++));do
-      ./release_fake_child_parent.sh;
-      ./fake_parent_helper --device_class=$class --application_id=$app_id --profile_variable \
-         >> fsmda_profiling.log &
-         sleep 0.5;
-      ./fake_child_helper --device_class=$class\
-         >> fsmda_profiling.log
-    done
-    echo -n "$class parent average="
-    grep "fsmda_parent profile_variable $class" < fsmda_profiling.log \
-      |awk '{ SUM += $4; N += 1} END { printf "%s\n",SUM/N }'
-done
-
-
-
+### final release
+./release_fake_child_parent.sh  && ./release_gtest.sh
 
